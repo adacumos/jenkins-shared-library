4982d0740741d8a364b4458e2ac08049102d645e
Arvin Dacumos <adacumos@protonmail.com>
2026-09-10 23:20:49 -0700

    Added parameter arguments to buildImage()

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building the docker image...."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', 
     usernameVariable: 'USER', 
     passwordVariable: 'PASS')]) {
         sh 'echo ${PASS} | docker login -u ${USER} --password-stdin'
-        sh 'docker build -t ${DOCKER_IMAGE} .'
-        sh 'docker push ${DOCKER_IMAGE}'
+        sh "docker build -t $imageName ."
+        sh "docker push $imageName"
     }
 }
